f25529b973f751502506f567cd3da4b36d74e939
LadySmaell <156449593+LadySmaell@users.noreply.github.com>
2026-09-25 12:09:29 +0100

    Update reviews.js

--- reviews.js
+++ reviews.js
@@ -31,7 +31,7 @@ reviews.forEach(review => {
                 <li>🎮 ${review.genre}</li>
                 <li>⏱ ${review.size}</li>
                 <li>🏆 ${review.achievements}</li>
-                <li>📅 ${📅 ${
+                <li>📅 ${
     new Date(review.published)
         .toLocaleDateString(
             "en-GB",
